2c7807f221166bda00ac19a9849eda1be73bd607
qefeng <qefeng@tencent.com>
2021-01-13 19:07:50 +0800

    doc: 修复文档配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,14 +13,14 @@ module.exports = {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
     '/': {
-      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
-      title: 'figma-design-token-dock',
-      description: 'Vue-powered Static Site Generator'
-    },
-    '/en': {
-      lang: 'zh-CN',
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
       title: 'figma-design-token-dock',
       description: 'Vue 驱动的静态网站生成器'
+    },
+    '/en': {
+      lang: 'en-US',
+      title: 'figma-design-token-dock',
+      description: 'Vue-powered Static Site Generator'
     }
   },
   themeConfig: {
