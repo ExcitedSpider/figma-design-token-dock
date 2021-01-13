20d6040c424c276923ef8e39d7a1cb8de2056b8c
qefeng <qefeng@tencent.com>
2021-01-13 17:58:20 +0800

    docs: 文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+const getConfig = require("vuepress-bar");
+const root = `${__dirname}/..`;
+
+const sidebarConfig = getConfig(root, { addReadMeToFirstGroup: false }).sidebar;
+
+module.exports = {
+  head: [],
+  base: '/figma-design-token-dock',
+  markdown: {
+    plugins: ['markdown-it-task-lists']
+  },
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
+      title: 'figma-design-token-dock',
+      description: 'Vue-powered Static Site Generator'
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'figma-design-token-dock',
+      description: 'Vue 驱动的静态网站生成器'
+    }
+  },
+  themeConfig: {
+    repo: 'https://github.com/ExcitedSpider/figma-design-token-dock',
+    sidebarDepth: 6,
+    /** 开放搜索 */
+    search: true,
+    searchMaxSuggestions: 10,
+    /** 禁用上一篇/下一篇 */
+    nextLinks: false,
+    prevLinks: false,
+    /** 侧边栏导航配置 */
+    sidebar: 'auto',
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,22 @@
+# Design Token Dock
+
+This is a plugin to export figma style to design token json.
+
+## install
+
+install on [figma community](https://www.figma.com/community/plugin/903167004921142962/Design-Token-Dock-%2F-%E6%A0%B7%E5%BC%8F%E5%AF%BC%E5%87%BA%E5%B7%A5%E5%85%B7)
+
+## Usage
+
+Start & use plugin on figma menu.
+
+## development
+
+1. clone & install
+
+  ```bash
+  npm i
+  npm run start   
+  ```
+
+2. load `manifest.json` in figma desktop
